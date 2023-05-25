60c9779332f4493604bfb9ac0765a68576a9a2f7
Moinul Moin <moinulmoin101@gmail.com>
2023-05-25 14:21:56 +0600

    refactor: add introducing tweet link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default async function Home() {
 		<div className="container z-10">
 			<div className=" w-full max-w-2xl mx-auto">
 				<a
-					href="https://twitter.com/immoinulmoin"
+					href="https://twitter.com/immoinulmoin/status/1661645764697919489"
 					target="_blank"
 					rel="noreferrer"
 					className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200"
