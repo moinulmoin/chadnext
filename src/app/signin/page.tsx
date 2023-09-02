3f6f7a73f57e6092baad2de1ca9c663435a9bd6d
Moinul Moin <moinulmoin101@gmail.com>
2023-09-02 23:36:11 +0600

    feat: improve signin page ui

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,16 +1,17 @@
 import AuthForm from "~/components/layout/auth-form";
+import { Card } from "~/components/ui/card";
 
 export default async function Signin() {
   return (
-    <section className=" bg-transparent">
-      <div className=" container">
+    <section>
+      <div className="container ">
         <div className="flex min-h-[calc(100vh-184px)] items-center justify-center md:min-h-[calc(100vh-160px)]">
-          <div className="w-full max-w-[400px]">
+          <Card className="w-full max-w-[450px] p-6 shadow-md">
             <h2 className="pb-2 text-center text-3xl font-semibold tracking-tight transition-colors">
               Welcome Back 👋
             </h2>
             <AuthForm />
-          </div>
+          </Card>
         </div>
       </div>
     </section>
