22c554c0613de4ffe69fef0ac96b2acc0a95e73d
Moinul Moin <moinulmoin101@gmail.com>
2023-10-12 22:21:02 +0600

    refactor: change toast position to top right

--- src/components/ui/use-toast.ts
+++ src/components/ui/use-toast.ts
@@ -3,8 +3,8 @@ import * as React from "react";
 
 import type { ToastActionElement, ToastProps } from "~/components/ui/toast";
 
-const TOAST_LIMIT = 1;
-const TOAST_REMOVE_DELAY = 1000000;
+const TOAST_LIMIT = 5;
+const TOAST_REMOVE_DELAY = 5000;
 
 type ToasterToast = ToastProps & {
   id: string;
@@ -186,4 +186,4 @@ function useToast() {
   };
 }
 
-export { useToast, toast };
+export { toast, useToast };
