31ba942436251224c37059730edc907805bf2479
Moinul Moin <moinulmoin101@gmail.com>
2023-12-05 23:27:05 +0600

    refactor: add menu for mobile and improve the header

--- src/components/layout/header/navbar.tsx
+++ src/components/layout/header/navbar.tsx
@@ -1,14 +1,19 @@
+"use client";
+
+import { MenuIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 import { buttonVariants } from "~/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "~/components/ui/sheet";
 import { type CurrentUser } from "~/types";
 import UserNav from "../user-nav";
-
 export default function Navbar({
   loggedInUser,
 }: {
   loggedInUser: CurrentUser;
 }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <nav className="flex h-full items-center justify-between">
       <Link href="/" className="flex items-center text-2xl font-bold">
@@ -21,7 +26,7 @@ export default function Navbar({
         />
         <p>ChadNext</p>
       </Link>
-      <div className="flex items-center gap-12 2xl:gap-16">
+      <div className="hidden items-center gap-12 md:flex 2xl:gap-16">
         <div className="space-x-4 text-center text-sm leading-loose text-muted-foreground md:text-left">
           <Link
             href="/changelog"
@@ -46,6 +51,40 @@ export default function Navbar({
           )}
         </div>
       </div>
+      <Sheet open={isModalOpen} onOpenChange={setIsModalOpen}>
+        <SheetTrigger className="md:hidden">
+          <MenuIcon />
+        </SheetTrigger>
+        <SheetContent>
+          <div className="flex flex-col items-center space-y-10 py-10">
+            <div className="space-y-4 text-center text-sm leading-loose text-muted-foreground">
+              <Link
+                href="/changelog"
+                className="block font-semibold hover:underline hover:underline-offset-4"
+                onClick={() => setIsModalOpen(false)}
+              >
+                Changelog
+              </Link>
+              <Link
+                href="/about"
+                className="block font-semibold hover:underline hover:underline-offset-4"
+                onClick={() => setIsModalOpen(false)}
+              >
+                About
+              </Link>
+            </div>
+            <div className="flex items-center gap-x-2">
+              {loggedInUser ? (
+                <UserNav user={loggedInUser} />
+              ) : (
+                <Link href="/signin" className={buttonVariants()}>
+                  Sign In
+                </Link>
+              )}
+            </div>
+          </div>
+        </SheetContent>
+      </Sheet>
     </nav>
   );
 }
