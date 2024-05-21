b87757607701f88754d2ec0ab7fa74ce3648d879
Moinul Moin <moinulmoin101@gmail.com>
2024-05-21 23:29:06 +0600

    feat: :sparkles: replace contentlayer with velite

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-const { withContentlayer } = require("next-contentlayer");
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
   disable: process.env.NODE_ENV !== "production",
@@ -18,4 +17,4 @@ const nextConfig = {
   trailingSlash: true,
 };
 
-module.exports = withPWA(withContentlayer(nextConfig));
+module.exports = withPWA(nextConfig);
