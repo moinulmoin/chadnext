5c5b92b84a7113e6de93c83d7f5e0612b1c03be2
Moinul Moin <moinulmoin101@gmail.com>
2024-07-11 21:50:23 +0600

    fix: :bug: trying to fix ut callback failling

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -1,6 +1,7 @@
 import { createRouteHandler } from "uploadthing/next";
-
 import { ourFileRouter } from "./core";
+
+export const runtime = "nodejs";
 
 // Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
